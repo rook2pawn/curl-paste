6897ede41782c2c6dcb4f7d0b7f49a45aa98b691
David Wee <rook2pawn@gmail.com>
2015-05-04 12:59:02 -0700

    update

--- test/test.js
+++ test/test.js
@@ -1,5 +1,8 @@
-var store = require('../lib/store')();
-
+var store = require('../lib/store')({
+    duration: 1000*60*60*168,
+    checkfrequency: 1000*60
+})
+var handle = require('../lib/handle')(store)
 var inputhash = { 
     a : {val: 'foo', timestamp:1349 },
     b : {val: 'foo', timestamp:1351 },
@@ -7,5 +10,24 @@ var inputhash = {
     d : {val: 'foo', timestamp:1550 },
     e : {val: 'foo', timestamp:1600 }
 }
-
 console.log(store.clean(inputhash,1700,160))
+
+var request = require('supertest')
+request(handle.request)
+  .get('/')
+  .expect(404)
+  .expect('File not found. :(')
+  .end(function(err, res){
+    if (err) throw err;
+  });
+
+
+request(handle.request)
+  .get('/')
+  .expect(404)
+  .expect('File not found. :(')
+  .end(function(err, res){
+    if (err) throw err;
+  });
+
+
