e1dc45e7358483d81c7a625266ce444d9dc8f6ff
David Wee <rook2pawn@gmail.com>
2015-05-08 05:24:09 -0700

    fixed test

--- test/test.js
+++ test/test.js
@@ -23,19 +23,27 @@ test('get',function(t) {
 });
 
 test('post and then get',function(t) {
-  var my_url;
+  var raw_url;
+  var web_url;
   var agent = request(handle.request)
   t.test('first part post',function(st) {
     agent
     .post('/')
     .send('foobar')
     .expect(function(res) {
-      var _url = res.text.slice(0,-1)
-      if (vu.isUri(_url) === false) {
-        st.fail("not valid url "+_url)
+      var lines = res.text.split('\n');
+      raw_url = lines[0].slice(4)
+      web_url = lines[1].slice(4)
+      if (vu.isUri(raw_url) === false) {
+        st.fail("not valid url "+raw_url)
       } else {
-        st.pass("valid url "+_url)
-        my_url = _url; 
+        st.pass("valid url "+raw_url)
+      }
+
+      if (vu.isUri(web_url) === false) {
+        st.fail("not valid url "+web_url)
+      } else {
+        st.pass("valid url "+web_url)
       }
     })
     .end(function(err,res) {
@@ -45,13 +53,14 @@ test('post and then get',function(t) {
   })
 
   t.test('second part get',function(st) {
-    var href = url.parse(my_url).path;
+    var href = url.parse(raw_url).path;
     agent
     .get(href)
     .expect(function(res) {
 // if you uncomment the following line, npm test will fail
 // but tape test/test.js will work ?
 //      st.comment("got text:" + res.text)
+      console.log("getting ", href, " got back ->",res.text)
       st.equal(res.text, 'foobar\n')
     })
     .end(function(err,res) {
